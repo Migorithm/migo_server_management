b8b98137e59df59c921e499628b4e6172705e176
Migo <saka90030@gmail.com>
2022-02-17 18:55:11 +0900

    First commit

---
+++ .flaskenv
@@ -0,0 +1,7 @@
+FLASK_APP=hello.py
+SERVERS='["10.107.11.66","10.107.11.59"]'
+FLASK_RUN_PORT=5000
+FLASK_RUN_HOST=0.0.0.0
+FLASK_DEBUG=True
+admins='["sykim@wemakeprice.com","migo@wemakeprice.com"]'
+SECRET_KEY="you-will-never-guess"
---
+++ hello.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, session, redirect, url_for,flash
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+import json,os
+from datetime import datetime 
+
+app = Flask(__name__)
+moment= Moment(app)
+bootstrap = Bootstrap(app)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+class NameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit= SubmitField("Submit")
+
+@app.route('/',methods=["GET","POST"])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name= session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Name change has been detected!")
+        session["name"]=form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html",form=form,name=session.get("name",None))
+
+@app.route('/description')
+def description():
+    return render_template("description.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    admins=json.loads(os.environ.get("admins"))
+    return render_template('404.html',admins=admins), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
