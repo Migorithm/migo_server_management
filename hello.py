d1795068c4647106f749f9b873cc1d3df384e66c
migo <saka90030@gmail.com>
2022-02-19 02:20:51 +0900

    dssd

--- hello.py
+++ hello.py
@@ -6,12 +6,18 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired,Length
 import json,os
 from datetime import datetime 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 moment= Moment(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate= Migrate(app,db)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
+
+#web forms 
 class NameForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired(),Length(1,20)])
     submit= SubmitField("Submit")
@@ -19,6 +25,23 @@ class NameForm(FlaskForm):
 class SearchForm(FlaskForm):
     searchbar = StringField("Search",validators=[DataRequired()])
     submit=SubmitField("Search")
+
+
+
+#models
+class Role(db.Model):
+    __tablename__ =  "roles"
+    name = db.Column(db.String(64),unique=True)
+    id = db.Column(db.Integer,primary_key=True)
+    users = db.relationship("User",backref="role",lazy="dynamic")
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+class User(db.Model):
+    __tablename__ = "users"
+    username = db.Column(db.String(64),index=True)
+    rold_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+    id=db.Column(db.Integer,primary_key=True)
 
 
 @app.route('/',methods=["GET","POST"])
@@ -47,3 +70,5 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
